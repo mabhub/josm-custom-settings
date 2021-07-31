c65b8f55946f184ab7ad6ad4e01cf790f0fb41d5
mab <git@ma-b.com>
2021-08-01 01:18:30 +0200

    Refactor & reorder processing

--- index.mjs
+++ index.mjs
@@ -15,22 +15,28 @@ const activities = process.env.ACTIVITIES.split(',');
 const colors = process.env.COLORS.split(',');
 
 (async () => {
-  const {
-    'CloudFront-Policy': policy,
-    'CloudFront-Key-Pair-Id': kpid,
-    'CloudFront-Signature': sign,
-  } = await getStravaCookies(`${process.env.FIREFOX_PROFiLE_DIR}cookies.sqlite`);
-
   const xml = await fs.readFile(josmPrefs);
-  const data = await xml2js.parseStringPromise(xml.toString());
+  const josm = await xml2js.parseStringPromise(xml.toString());
 
-  // Remove all Strava nagScreen dismissal
-  data.preferences.tag = data.preferences.tag.filter(({ '$': { key } }) => !key.includes('message.imagery.nagPanel.https://heatmap-external-{switch:a,b,c}.strava.com'))
-
-  const imageries = data.preferences.maps.find(({ '$': { key } }) => key === 'imagery.entries');
-  data.preferences.maps = data.preferences.maps.filter(({ '$': { key } }) => key !== 'imagery.entries');
+  // Extract imagery settings
+  const imageries = josm.preferences.maps.find(({ '$': { key } }) => key === 'imagery.entries');
 
   if (imageries) {
+    josm.preferences.maps = josm.preferences.maps.filter(
+      ({ '$': { key } }) => key !== 'imagery.entries'
+    );
+
+    // Remove all Strava nagScreen dismissal
+    josm.preferences.tag = josm.preferences.tag.filter(
+      ({ '$': { key } }) => !key.includes('message.imagery.nagPanel.https://heatmap-external-{switch:a,b,c}.strava.com')
+    );
+
+    const {
+      'CloudFront-Policy': policy,
+      'CloudFront-Key-Pair-Id': kpid,
+      'CloudFront-Signature': sign,
+    } = await getStravaCookies(`${process.env.FIREFOX_PROFiLE_DIR}cookies.sqlite`);
+
     // Remove previous Strava layers
     imageries.map = imageries.map.filter(
       ({ tag }) => tag?.every(({ '$': { value } }) => !value.includes('strava'))
@@ -43,16 +49,15 @@ const colors = process.env.COLORS.split(',');
         imageries.map.push(makeTms(`Strava ${activity} ${color}`, tms));
 
         // Dismiss imagery.nagPanel
-        data.preferences.tag.push({
+        josm.preferences.tag.push({
           '$': { key: `message.imagery.nagPanel.${tms}`, value: 'false' },
         });
       });
     });
 
-    data.preferences.maps.push(imageries);
+    josm.preferences.maps.push(imageries);
   }
 
   const builder = new xml2js.Builder();
-  const xml2 = builder.buildObject(data);
-  await fs.writeFile(josmPrefs, xml2);
+  await fs.writeFile(josmPrefs, builder.buildObject(josm));
 })();
